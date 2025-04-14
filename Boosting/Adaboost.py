c4769c2320b7afb7b0264f8cd121b4eac57d5682
Omar Hashem <114051743+omarhashem80@users.noreply.github.com>
2025-04-14 02:58:35 +0200

    Update Adaboost.py

--- Boosting/Adaboost.py
+++ Boosting/Adaboost.py
@@ -12,7 +12,7 @@ class Adaboost:
         m = x_train.shape[0]
         
         # TODO 1: Initialize the weights of each point in the training set to 1/m
-        W = 1 / m                            # should have shape (m,)
+        W = np.full((m,), 1 / m)                            # should have shape (m,)
 
         # loop over the boosting iterations 
         for t, weak_clf in enumerate(self.weak_clfs):
@@ -21,27 +21,26 @@ class Adaboost:
             # read the docs of the fit method in sklearn.tree.DecisionTreeClassifier to see how the weights can be passed
             weak_clf.fit(x_train, y_train, sample_weight=W)
             # TODO 3: Compute the indicator function Iₜ for each point. This is a (m,) array of 0s and 1s.
-            hₜ = weak_clf.predict
-            Iₜ = None
+            hₜ = weak_clf.predict(x_train)
+            Iₜ = y_train != hₜ
             
             # TODO 4: Use the indicator function Iₜ in boolean masking to compute the error
-            errₜ =  None
-
+            errₜ =  np.sum(W * Iₜ, axis=0)
             # TODO 5: Compute the estimator coefficient αₜ
-            αₜ = None
+            αₜ = np.log((1.0 - errₜ) / errₜ)
             self.αs.append(αₜ)                  
 
             # TODO 6: Update the weights using the estimator coefficient αₜ and the indicator function Iₜ
-            W = None
+            W = W * np.exp(αₜ * Iₜ)
             
             # TODO 7: Normalize the weights
-            W = None
+            W = W / np.sum(W, axis=0)
 
         return self
     
     def predict(self, x_val):
         # TODO 8: Compute a (T, m) array of predictions that maps each estimator to its predictions of x_val weighted by its alpha
-        weighted_opinions = np.array(None)     # Use zip
+        weighted_opinions = np.array([αₜ * weak_clf.predict(x_val) for αₜ , weak_clf in zip(self.αs, self.weak_clfs)])     # Use zip
         # Now have T evaluations of x_val each weighted (multiplied) by the corresponding alpha, 
         # so as per the formula we only need to take the sign of the sum of the different evaluations
         return np.sign(np.sum(weighted_opinions, axis=0))
